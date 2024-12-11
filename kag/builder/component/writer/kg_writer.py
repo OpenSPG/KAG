4ff42703aae3f0fe1ae2645a8ba7a8876aacb5eb
锦呈 <zhangxinhong.zxh@antgroup.com>
2024-12-11 15:11:19 +0800

    rename graphalgoclient to graphclient

--- kag/builder/component/writer/kg_writer.py
+++ kag/builder/component/writer/kg_writer.py
@@ -14,7 +14,7 @@ import os
 from enum import Enum
 from typing import Type, Dict, List
 
-from knext.graph_algo.client import GraphAlgoClient
+from knext.graph.client import GraphClient
 from kag.builder.model.sub_graph import SubGraph
 from kag.interface.builder.writer_abc import SinkWriterABC
 from knext.common.base.runnable import Input, Output
@@ -39,7 +39,7 @@ class KGWriter(SinkWriterABC):
     def __init__(self, project_id: str = None, **kwargs):
         super().__init__(**kwargs)
         self.project_id = project_id or os.getenv("KAG_PROJECT_ID")
-        self.client = GraphAlgoClient(project_id=project_id)
+        self.client = GraphClient(project_id=project_id)
 
     @property
     def input_types(self) -> Type[Input]:
@@ -50,7 +50,10 @@ class KGWriter(SinkWriterABC):
         return None
 
     def invoke(
-        self, input: Input, alter_operation: str = AlterOperationEnum.Upsert, lead_to_builder: bool = False
+        self,
+        input: Input,
+        alter_operation: str = AlterOperationEnum.Upsert,
+        lead_to_builder: bool = False,
     ) -> List[Output]:
         """
         Invokes the specified operation (upsert or delete) on the graph store.
@@ -63,7 +66,11 @@ class KGWriter(SinkWriterABC):
         Returns:
             List[Output]: A list of output objects (currently always [None]).
         """
-        self.client.write_graph(sub_graph=input.to_dict(), operation=alter_operation, lead_to_builder=lead_to_builder)
+        self.client.write_graph(
+            sub_graph=input.to_dict(),
+            operation=alter_operation,
+            lead_to_builder=lead_to_builder,
+        )
         return [None]
 
     def _handle(self, input: Dict, alter_operation: str, **kwargs):
