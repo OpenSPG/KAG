88dbf6464d8f55c6502ed223c8a96c6d418b709f
FishJoy <chengqiang.cq@antgroup.com>
2024-12-13 13:53:10 +0800

    DEMO

--- kag/solver/main_solver.py
+++ kag/solver/main_solver.py
@@ -9,17 +9,13 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License
 # is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 # or implied.
+import json
 import os
 
-from kag.common.llm.client import LLMClient
-from kag.solver.common.base import Question
-from kag.solver.implementation.default_kg_retrieval import KGRetrieverByLlm
-from kag.solver.implementation.default_lf_planner import DefaultLFPlanner
-from kag.solver.implementation.default_reasoner import DefaultReasoner
-from kag.solver.implementation.lf_chunk_retriever import LFChunkRetriever
-from kag.solver.logic.core_modules.lf_solver import LFSolver
-from kag.solver.logic.solver_pipeline import SolverPipeline
+from knext.project.client import ProjectClient
+
 from kag.solver.tools.info_processor import ReporterIntermediateProcessTool
+from kag_ant.thinker.thinker import Thinker
 
 
 class SolverMain:
@@ -27,26 +23,21 @@ class SolverMain:
     def invoke(self, project_id: int, task_id: int, query: str, report_tool=True, host_addr="http://127.0.0.1:8887"):
         # resp
         report_tool = ReporterIntermediateProcessTool(report_log=report_tool, task_id=task_id, project_id=project_id, host_addr=host_addr)
+        query_obj = json.loads(query)
+        config = ProjectClient(host_addr=host_addr, project_id=project_id).get_config(project_id)
 
-        lf_planner = DefaultLFPlanner(KAG_PROJECT_ID=project_id, KAG_PROJECT_HOST_ADDR=host_addr)
-        lf_solver = LFSolver(
-            kg_retriever=KGRetrieverByLlm(KAG_PROJECT_ID=project_id, KAG_PROJECT_HOST_ADDR=host_addr),
-            chunk_retriever=LFChunkRetriever(KAG_PROJECT_ID=project_id, KAG_PROJECT_HOST_ADDR=host_addr),
-            report_tool=report_tool,
-            KAG_PROJECT_ID=project_id,
-            KAG_PROJECT_HOST_ADDR=host_addr
-        )
-        reason = DefaultReasoner(lf_planner=lf_planner, lf_solver=lf_solver, KAG_PROJECT_ID=project_id, KAG_PROJECT_HOST_ADDR=host_addr)
-        question = Question(query)
-        question.id = 0
-        resp = SolverPipeline(reasoner=reason, KAG_PROJECT_ID=project_id, KAG_PROJECT_HOST_ADDR=host_addr)
-        answer, trace_log = resp.run(query)
-        print(trace_log)
-        report_tool.report_node(question, answer, ReporterIntermediateProcessTool.STATE.FINISH)
-        return answer
+        llm_config = eval(os.getenv("KAG_LLM", "{}"))
+        llm_config.update(config.get("llm", {}))
+        thinker = Thinker(env="dev", llm_config=llm_config)
+        response = thinker.run_folio(context=query_obj['context'], question=query_obj['question'])
+        report_tool.report_markdown(response['thinkerCot'])
+        return response['thinkerCot']
 
 
 if __name__ == "__main__":
-    res = SolverMain().invoke(3, 283, "周杰伦在哪一年基于什么作品获得的全球畅销专辑榜”冠军的华语歌手", True, host_addr="http://127.0.0.1:8887")
+    context="All people who regularly drink coffee are dependent on caffeine. People either regularly drink coffee or joke about being addicted to caffeine. No one who jokes about being addicted to caffeine is unaware that caffeine is a drug. Rina is either a student and unaware that caffeine is a drug, or neither a student nor unaware that caffeine is a drug. If Rina is not a person dependent on caffeine and a student, then Rina is either a person dependent on caffeine and a student, or neither a person dependent on caffeine nor a student."
+    question="Based on the above information, is the following statement true, false, or uncertain? Rina is either a person who jokes about being addicted to caffeine or is unaware that caffeine is a drug."
+    query = {'context': context, 'question': question}
+    res = SolverMain().invoke(1500002, 900001, json.dumps(query), True, host_addr="http://6.1.162.160:8887")
     print("*" * 80)
     print("The Answer is: ", res)
