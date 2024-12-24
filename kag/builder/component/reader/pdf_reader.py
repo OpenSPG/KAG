4bc717ad67b5d91b83d6963d3f8da28058607273
zhuzhongshu123 <zhongshu.zzs@antgroup.com>
2024-12-24 13:45:55 +0800

    fix reader init

--- kag/builder/component/reader/pdf_reader.py
+++ kag/builder/component/reader/pdf_reader.py
@@ -24,7 +24,7 @@ from kag.builder.prompt.outline_prompt import OutlinePrompt
 from kag.interface import LLMClient
 from kag.common.conf import KAG_PROJECT_CONF
 from kag.common.utils import generate_hash_id
-from knext.common.base.runnable import Input, Output
+from knext.common.base.runnable import Output
 from pdfminer.high_level import extract_text
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer, LTPage
@@ -48,14 +48,19 @@ class PDFReader(ReaderABC):
     It inherits from `ReaderABC` and overrides the necessary methods to handle PDF-specific operations.
     """
 
-    def __init__(self, **kwargs):
+    def __init__(
+        self,
+        cut_depth: int = 3,
+        llm: LLMClient = None,
+        is_ocr: bool = False,
+        **kwargs,
+    ):
         super().__init__(**kwargs)
-        self.split_level = kwargs.get("split_level", 3)
+        self.cut_depth = cut_depth
         self.outline_flag = True
-        self.llm = kwargs.get("llm", None)
-        language = os.getenv("KAG_PROMPT_LANGUAGE", "zh")
-        self.prompt = OutlinePrompt(language)
-        self.is_ocr = kwargs.get("is_ocr", False)
+        self.llm = llm
+        self.prompt = OutlinePrompt(KAG_PROJECT_CONF.language)
+        self.is_ocr = is_ocr
 
     @property
     def input_types(self):
@@ -355,7 +360,6 @@ class PDFReader(ReaderABC):
                 self.outline_flag = False
 
             if not self.outline_flag:
-
                 with open(input, "rb") as file:
                     for idx, page_layout in enumerate(extract_pages(file)):
                         content = ""
