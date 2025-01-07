cb57427bceac8dea7d11a01a7480908a73a33c65
zhuzhongshu123 <152354526+zhuzhongshu123@users.noreply.github.com>
2025-01-07 19:23:51 +0800

    fix knext client (#229)

--- knext/common/base/client.py
+++ knext/common/base/client.py
@@ -13,7 +13,6 @@ import os
 from abc import ABC
 
 from knext.common import rest
-from knext.common.env import env
 
 
 class Client(ABC):
@@ -40,8 +39,8 @@ class Client(ABC):
             host_addr (str): The address of the component server. If not provided, the value from the environment variable `KAG_PROJECT_HOST_ADDR` is used.
             project_id (int): The ID of the user's project. If not provided, the value from the environment variable `KAG_PROJECT_ID` is used.
         """
-        self._host_addr = host_addr or env.host_addr
-        self._project_id = project_id or env.id
+        self._host_addr = host_addr
+        self._project_id = project_id
 
     @staticmethod
     def serialize(obj):
