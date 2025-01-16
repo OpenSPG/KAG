fc95c4a93f88f0324a3f4a77d0692ff71aa17e41
huaidong.xhd <huaidong.xhd@antgroup.com>
2025-01-16 17:49:19 +0800

    fix encoding in markdown_reader.py to support windows

--- kag/builder/component/reader/markdown_reader.py
+++ kag/builder/component/reader/markdown_reader.py
@@ -517,7 +517,7 @@ class MarkDownReader(ReaderABC):
         if not os.path.isfile(file_path):
             raise FileNotFoundError(f"The file {file_path} does not exist.")
 
-        with open(file_path, "r") as reader:
+        with open(file_path, "r", encoding="utf-8") as reader:
             content = reader.read()
 
         basename, _ = os.path.splitext(os.path.basename(file_path))
