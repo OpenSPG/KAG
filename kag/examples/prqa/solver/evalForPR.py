577eea11a5ec6dc29fda2df94e07d53430a4c47d
wanxingyu.wxy <wanxingyu.wxy@antgroup.com>
2025-02-20 20:45:26 +0800

    1、优化logic_form_plan 2、修改spo_retrieval的问题 3、多线程处理qa问题

--- kag/examples/prqa/solver/evalForPR.py
+++ kag/examples/prqa/solver/evalForPR.py
@@ -1,77 +1,98 @@
 import json
-
-import pandas as pd
-from kag.common.conf import KAG_CONFIG
-from kag.solver.logic.solver_pipeline import SolverPipeline
-from kag.common.registry import import_modules_from_path
-from openpyxl import load_workbook
+import logging
 import os
-import openpyxl
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-# def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None, **to_excel_kwargs):
-#     try:
-#         # Load existing workbook
-#         book = load_workbook(filename)
-#
-#         # Create an ExcelWriter with the engine openpyxl
-#         with pd.ExcelWriter(filename, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-#             # This is now managed internally, and you do not need to set writer.book
-#             writer.sheets = {ws.title: ws for ws in book.worksheets}
-#
-#             if not startrow and sheet_name in writer.sheets:
-#                 startrow = writer.sheets[sheet_name].max_row
-#
-#             if startrow is None:
-#                 startrow = 0
-#
-#             df.to_excel(writer, sheet_name=sheet_name, startrow=startrow, header=not startrow, **to_excel_kwargs)
-#
-#     except FileNotFoundError:
-#         # If file does not exist, this will create it
-#         df.to_excel(filename, index=False, sheet_name=sheet_name, **to_excel_kwargs)
+from tqdm import tqdm
+
+from kag.common.conf import KAG_CONFIG
+from kag.common.registry import import_modules_from_path
+from kag.solver.logic.solver_pipeline import SolverPipeline
+
+logger = logging.getLogger(__name__)
+
+
+def load_processed_ids(progress_file):
+    if os.path.exists(progress_file):
+        with open(progress_file, 'r', encoding='utf-8') as f:
+            return set(json.load(f))
+    return set()
+
+
+def save_processed_id(id, progress_file):
+    processed_ids = load_processed_ids(progress_file)
+    processed_ids.add(id)
+    with open(progress_file, 'w', encoding='utf-8') as f:
+        json.dump(list(processed_ids), f)
+
 
 def qa(query):
     resp = SolverPipeline.from_config(KAG_CONFIG.all_config["kag_solver_pipeline"])
-    answer, traceLog = resp.run(query)
 
-    print(f"\n\nso the answer for '{query}' is: {answer}\n\n")  #
-    print(traceLog)
-    return answer, traceLog
+    try:
+        query_id = query.get("id")
+        to_query = query.get("question")
 
+        answer, trace_log = resp.run(to_query)
+        print(f"\n\nso the answer for '{to_query}' is:\n {answer}\n\n")
+        print(trace_log)
+        return query_id, answer, trace_log
+    except (ValueError, TypeError) as e:  # 捕获具体的异常
+        import traceback
+        logger.warning(
+            f"process sample failed with known error: {e}\n{traceback.format_exc()}\nfor: {data}"
+        )
+        return None
+    except Exception as e:  # 兜底捕获未知异常
+        import traceback
+        logger.error(
+            f"process sample failed with unexpected error: {e}\n{traceback.format_exc()}\nfor: {data}"
+        )
+        return None
+
+
+def execute_parallel_qa(qa_file_path, res_file_path, thread_num=1, upper_limit=10):
+    with open(qa_file_path, "r") as f:
+        qa_list = json.load(f)
+
+    processed_count = 0  # 计数器
+    with ThreadPoolExecutor(max_workers=thread_num) as executor:
+        futures = [
+            executor.submit(qa, query)
+            for query_id, query in enumerate(qa_list[:upper_limit])
+        ]
+        for future in tqdm(
+                as_completed(futures),
+                total=len(futures),
+                desc="parallel Qa for PRQA completing: ",
+        ):
+            result = future.result()
+            if result is not None:
+                query_id, answer, trace_log = result
+                query_id = int(query_id)
+                sample = qa_list[query_id]
+                sample["answer"] = answer
+                sample["trace_log"] = trace_log
+
+                processed_count += 1
+                if processed_count % 2 == 0:
+                    with open(res_file_path, "a", encoding="utf-8") as file:
+                        json.dump(qa_list, file, ensure_ascii=False)
+
+    with open(res_file_path, "a", encoding="utf-8") as file:
+        json.dump(qa_list, file, ensure_ascii=False)
 
 
 if __name__ == "__main__":
     import_modules_from_path("./prompt")
-
     json_file_path = "./data/test.json"
+    progress_file = "./progress21.json"
 
-    # with open(json_file_path, "r") as f:
-    #     queries = json.load(f)
+    execute_parallel_qa(json_file_path, progress_file, thread_num=5, upper_limit=202)
 
-    # for item in queries:
-    #     id = item.get("id")
-    #     question = item.get("question")
-    #     source = item.get("source")
-    #     print(f"query {id}: {question}")
-    #     answer, trace_log = qa(question)
-    #     data = {
-    #         'id': id,
-    #         'query': question,
-    #         "answer": answer,
-    #         "trace_log": trace_log
-    #     }
-    #     with open("queries_output.txt", "a", encoding="utf-8") as file:
-    #         # 将数据写入文件，每个字段一行
-    #         file.write(f"id: {data['id']}\n")
-    #         file.write(f"query: {data['query']}\n")
-    #         file.write(f"answer: {data['answer']}\n")
-    #         file.write(f"trace_log: {data['trace_log']}\n")
-    #         file.write("\n")  # 每个记录之间添加一个空行
+    # question = "《利箭纵横》的主要演员的哪位搭档与陈道明搭档过？"
+    # qa_answer, traceLog = qa(question)
+    # print(f"Question: {question}")
+    # print(f"Answer: {qa_answer}")
+    # print(f"TraceLog: {traceLog}")
 
-
-
-    query = "《酬韦相公见寄》的作者的某一个好友的好友张蠙除外, 请问他的好友还有谁?"
-    answer, trace_log = qa(query)
-    # print(f"Question: {query}")
-    # print(f"Answer: {answer}")
-    # print(f"TraceLog: {trace_log}")
