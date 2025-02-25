3a29ff5bfe3aac8b893e87e12bf8be697daba3b4
Donghai <donghai.ydh@antgroup.com>
2025-02-25 20:13:15 +0800

    update

--- kag/examples/finqa/solver/prompt/resp_generator.py
+++ kag/examples/finqa/solver/prompt/resp_generator.py
@@ -11,29 +11,38 @@ logger = logging.getLogger(__name__)
 @PromptABC.register("table_resp_generator")
 class FinQARespGenerator(PromptABC):
     template_zh = """
+# Task
 基于给定的信息回答问题。
-输出理由，最终直接给出数字答案，以`Finanl Answer: <number>`作为结束。
+先输出理由，最终给出数值答案，以`Finanl Answer: <number>`作为结束。
 
-给定的信息：
+# 注意事项
+如果问百分比，给出带有百分号的数值，如`Final Answer: 0.5%`。
+数值精度要求越高越好。
+
+# 给定的信息
+问题：'$instruction'
 ```
 $memory
 ```
-问题：'$instruction'
 
-你的答案：
+# 你的答案
 """
     template_en = """
-Answer the question based on the given information.
-Give the reason, and finally give the number answer directly, ending with `Final Answer: <number>`.
+# Task
+Based on the given information, answer the question.
+First, provide the reasoning, and then give the final numerical answer, ending with `Final Answer: <number>`.
 
-The following are given information:
+# Notes
+If the question asks for a percentage, provide the value with a `%` sign, such as `Final Answer: 0.5%`.
+Ensure the numerical precision is as high as possible.
+
+# Given Information
+Question: '$instruction'
 ```
 $memory
 ```
 
-Question: '$instruction'
-
-Your answer:
+# Your Answer
 """
 
     @property
