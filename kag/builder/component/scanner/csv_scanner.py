4a5d16b2be172766a2a02990dd187ca6680bbc18
zhuzhongshu123 <152354526+zhuzhongshu123@users.noreply.github.com>
2025-03-10 16:26:16 +0800

    add csv structured scanner (#402)

--- kag/builder/component/scanner/csv_scanner.py
+++ kag/builder/component/scanner/csv_scanner.py
@@ -86,3 +86,68 @@ class CSVScanner(ScannerABC):
                     )
 
         return contents
+
+
+@ScannerABC.register("csv_structured")
+@ScannerABC.register("csv_structured_scanner")
+class CSVStructuredScanner(ScannerABC):
+    def __init__(
+        self,
+        header: bool = True,
+        col_map: dict[int, str] = None,
+        rank: int = 0,
+        world_size: int = 1,
+    ):
+        super().__init__(rank=rank, world_size=world_size)
+        self.header = header
+        self.col_map = col_map
+
+    @property
+    def input_types(self) -> Input:
+        return str
+
+    @property
+    def output_types(self) -> Output:
+        return Dict
+
+    def load_data(self, input: Input, **kwargs) -> List[Output]:
+        """
+        Loads data from a CSV file and converts it into a list of dictionaries.
+
+        Args:
+            input (Input): The input file path to the CSV file.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            List[Output]: A list of dictionaries containing the processed data.
+        """
+        input = self.download_data(input)
+
+        if self.header:
+            data = pd.read_csv(input, dtype=str)
+        else:
+            data = pd.read_csv(input, dtype=str, header=None)
+
+        # 如果有多个分片，根据rank和world_size进行数据分片
+        if self.sharding_info.shard_count > 1:
+            total_rows = len(data)
+            shard_size = total_rows // self.sharding_info.shard_count
+            start_idx = self.sharding_info.shard_id * shard_size
+            end_idx = (
+                start_idx + shard_size
+                if self.sharding_info.shard_id < self.sharding_info.shard_count - 1
+                else total_rows
+            )
+            data = data.iloc[start_idx:end_idx]
+
+        if self.col_map is None:
+            return data.to_dict(orient="records")
+
+        contents = []
+        for _, row in data.iterrows():
+            renamed_row = {}
+            for old_key, new_key in self.col_map.items():
+                renamed_row[new_key] = row[old_key]
+            contents.append(renamed_row)
+
+        return contents
