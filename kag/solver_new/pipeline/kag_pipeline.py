ef99df1be609bc0bd12f67738a64d03bb1a29de7
钟书 <zhongshu.zzs@antgroup.com>
2025-03-12 11:55:11 +0800

    rename solver new

--- kag/solver_new/pipeline/kag_pipeline.py
+++ kag/solver_new/pipeline/kag_pipeline.py
@@ -1,18 +1,22 @@
-from http.client import responses
-from typing import Dict
-
-from kag.interface import SolverPipelineABC, PlannerABC, ExecutorABC, Context, TaskStatus
+from kag.interface import (
+    SolverPipelineABC,
+    PlannerABC,
+    ExecutorABC,
+    Context,
+    TaskStatus,
+)
 from kag.interface.solver.kag_generator_abc import KAGGeneratorABC
+
 
 """
 示例yaml
-kag_thought_pipeline:
-  type: kag_thought_pipeline
+kag_pipeline:
+  type: kag_pipeline
   planner:
     type: kag_planner
     llm_client: *chat_llm
     plan_prompt:
-        type: thought_plan_prompt
+        type: static_plan_prompt
     executors:
         - type: kag_hybrid_executor
                 entity_linking:
@@ -26,29 +30,38 @@ kag_thought_pipeline:
         - type: math_executor
   generator:
     type: kag_generator
-  max_thought_times: 5
+  max_reflect_time: 5
 """
 
-@SolverPipelineABC.register("kag_thought_pipeline")
-class KAGThoughtPipeline(SolverPipelineABC):
-    def __init__(self, planner: PlannerABC, generator: KAGGeneratorABC, max_thought_times):
+
+@SolverPipelineABC.register("kag_pipeline")
+class KAGPipeline(SolverPipelineABC):
+    def __init__(
+        self, planner: PlannerABC, generator: KAGGeneratorABC, max_reflect_time
+    ):
         super().__init__()
         self.planner = planner
         self.generator = generator
-        self.max_thought_times = max_thought_times
+        self.max_reflect_time = max_reflect_time
 
     def invoke(self, query, **kwargs):
-        thought_time = 0
+        reflect_time = 0
         context: Context = Context()
-        while thought_time < self.max_thought_times:
+        while reflect_time < self.max_reflect_time:
+            # TODO planner 默认返回的任务为一个dag，可能模型也只返回一个任务
             task_dag = self.planner.invoke(query, context=context, **kwargs)
+            is_finished = True
             for task in task_dag:
                 context.add_task(task)
+                # TODO 考虑从planner中拿到执行器？因为plan需要知道有哪些执行器，或者模型不感知有哪些执行器，由pipeline进行查找，比如只分为retriever、deduce、math？
                 executor: ExecutorABC = self.planner.get_executor_by_name(task.executor)
+                # TODO 此处调用执行器，但是返回值好像没有使用的必要，都在task中查找
                 executor.invoke(query, task, context, **kwargs)
+                # TODO 此为当前策略，当出现失败的时候，会进入反思模块，重新拆解问题
                 if task.status == TaskStatus.FAILED:
+                    is_finished = False
                     break
-
+            if is_finished:
+                break
         answer = self.generator.generate(query, context)
         return answer
-
--- kag/solver_new/planner/kag_planner.py
+++ kag/solver_new/planner/kag_planner.py
@@ -2,6 +2,7 @@ from typing import List
 
 from kag.interface import PlannerABC, Task, LLMClient, PromptABC
 
+
 @PlannerABC.register("kag_planner")
 class KAGPlanner(PlannerABC):
     def __init__(self, executors, llm_module: LLMClient, plan_prompt: PromptABC):
@@ -10,11 +11,14 @@ class KAGPlanner(PlannerABC):
         self.plan_prompt = plan_prompt
 
     def invoke(self, query, **kwargs) -> List[Task]:
-        plan_dict_dag = self.llm_module.invoke({
-            "query": query,
-            "context": kwargs.get("context"),
-            "functions": [executor.schema() for executor in self.executors]
-        }, self.plan_prompt)
+        plan_dict_dag = self.llm_module.invoke(
+            {
+                "query": query,
+                "context": kwargs.get("context"),
+                "functions": [executor.schema() for executor in self.executors],
+            },
+            self.plan_prompt,
+        )
         return self.create_tasks_from_dag(plan_dict_dag)
 
     def decompose_task(self, task: Task, **kwargs) -> List[Task]:
