2e3306879b05b5481cf0d662fa3f23357b8a9e08
peilong <peilong.zpl@antgroup.com>
2025-03-18 11:22:23 +0800

    add kag flow component test

--- tests/unit/executor/kag_hybrid_executor.py
+++ tests/unit/executor/kag_hybrid_executor.py
@@ -2,7 +2,7 @@ import asyncio
 from kag.interface import SolverPipelineABC
 from kag.common.conf import KAG_CONFIG
 from kag.interface import Task
-from kag.solver_new.executor.retriever.local_knowlege_base.kag_hybrid_executor import KagHybridExecutor
+from kag.solver_new.executor.retriever.local_knowlege_base.kag_retriever.kag_hybrid_executor import KagHybridExecutor
 
 async def qa(pipeline, query):
     result = await pipeline.ainvoke(query)
@@ -14,17 +14,17 @@ def do_pipeline_kag():
     print(f"pipeline_config = {pipeline_config}")
     pipeline = SolverPipelineABC.from_config(pipeline_config)
 
-    query = "When did the explorer reach the city where the headquarters of the only group larger than Vilaiyaadu Mankatha's record label is located?"
+    query = "周润发的爸爸和妈妈分别叫做什么名字"
     result = asyncio.run(qa(pipeline, query))
     print(result)
 
 def do_kag_hybrid():
     kag_executor = KagHybridExecutor.from_config(KAG_CONFIG.all_config['kag_hybrid_executor'])
     task = Task(executor="kag_hybrid_executor", arguments={
-            "query": "When did the explorer reach the city where the headquarters of the only group larger than Vilaiyaadu Mankatha's record label is located?"
+            "query": "周润发的父亲是谁"
         })
     res = kag_executor.invoke(
-        query="When did the explorer reach the city where the headquarters of the only group larger than Vilaiyaadu Mankatha's record label is located?",
+        query="周润发的父亲是谁",
         task=task,
         context={}
     )
