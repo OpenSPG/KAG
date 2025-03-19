9b7117262cb5ed94641a00f8e0202ff428e027c7
peilong <peilong.zpl@antgroup.com>
2025-03-19 10:20:25 +0800

    add stream reporter client

--- kag/interface/solver/reporter_abc.py
+++ kag/interface/solver/reporter_abc.py
@@ -1,9 +1,44 @@
+import logging
+import asyncio
+
 from kag.common.registry import Registrable
 
+logger = logging.getLogger()
 
 class ReporterABC(Registrable):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self._running = True
+        self._monitor_task_coroutine = None
 
-    def report(self, segment, tag_name, content, status):
+    def add_report_line(self, segment, tag_name, content, status):
         raise NotImplementedError()
+
+    def do_report(self):
+        raise NotImplementedError()
+
+
+    async def start(self):
+        self._monitor_task_coroutine = asyncio.create_task(self.do_cycle_report())
+        logging.info("reporter is starting")
+
+    async def stop(self):
+        """
+        停止任务
+        """
+        self._running = False
+        if self._monitor_task_coroutine:
+            await self._monitor_task_coroutine
+        logging.info("reporter is stop")
+    async def do_cycle_report(self):
+        try:
+            while self._running:
+                # 定期获取数据
+                await asyncio.sleep(1)
+
+                self.do_report()
+            self.do_report()
+        except asyncio.CancelledError:
+            logging.info("reporter is cancel")
+        finally:
+            self._running = False
