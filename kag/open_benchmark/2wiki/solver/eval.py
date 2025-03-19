2d1a7c718b66b3c241c9db49c546c8df4736b5d8
peilong <peilong.zpl@antgroup.com>
2025-03-19 15:36:31 +0800

    commit with benchmark sh

--- kag/open_benchmark/2wiki/solver/eval.py
+++ kag/open_benchmark/2wiki/solver/eval.py
@@ -11,13 +11,13 @@ from kag.common.registry import import_modules_from_path
 logger = logging.getLogger(__name__)
 
 
-class EvaForHotPotQa(EvalQa):
+class EvaFor2wiki(EvalQa):
     """
     init for kag client
     """
 
     def __init__(self, solver_pipeline_name="kag_solver_pipeline"):
-        super().__init__(task_name="hotpotqa", solver_pipeline_name=solver_pipeline_name)
+        super().__init__(task_name="2wiki", solver_pipeline_name=solver_pipeline_name)
 
     def load_data(self, file_path):
         with open(file_path, "r") as f:
@@ -34,6 +34,6 @@ if __name__ == "__main__":
     # 解析命令行参数
     parser = running_paras()
     args = parser.parse_args()
-    qa_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), f"data/{args.qa_file}")
+    qa_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), f"{args.qa_file}")
     do_main(qa_file_path=qa_file_path, thread_num=args.thread_num, upper_limit=args.upper_limit,
-            collect_file=args.res_file, eval_obj=EvaForHotPotQa())
+            collect_file=args.res_file, eval_obj=EvaFor2wiki())
