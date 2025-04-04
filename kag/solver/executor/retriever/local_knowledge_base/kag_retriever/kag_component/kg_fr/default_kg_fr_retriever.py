245c215e92d91205158f38f49f085cd924fd27f4
peilong <peilong.zpl@antgroup.com>
2025-04-04 19:26:33 +0800

    support dot refresh

--- kag/solver/executor/retriever/local_knowledge_base/kag_retriever/kag_component/kg_fr/default_kg_fr_retriever.py
+++ kag/solver/executor/retriever/local_knowledge_base/kag_retriever/kag_component/kg_fr/default_kg_fr_retriever.py
@@ -49,7 +49,7 @@ class KgFreeRetrieverWithOpenSPG(KGFreeRetrieverABC):
                     reporter.add_report_line(
                         "thinker",
                         f"begin_sub_kag_retriever_{logic_node.sub_query}_{self.name}",
-                        "finsh",
+                        "finish",
                         "FINISH",
                         component_name=self.name
                     )
