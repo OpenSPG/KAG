223dd405a9101d5b2abadaf49d9b774174f8b754
zhengke.gzk <zhengke.gzk@antgroup.com>
2025-04-25 14:03:47 +0800

    musique_qa，supporting_facts_with_decomposition: {'processNum': 1000, 'em': 0.626, 'f1': 0.752, 'LLM-Accuracy': 0.823, 'cost': 1760.599}

--- kag/open_benchmark/musique/solver/eval_ideal_with_decomposition.py
+++ kag/open_benchmark/musique/solver/eval_ideal_with_decomposition.py
@@ -117,6 +117,7 @@ if __name__ == "__main__":
     import_modules_from_path("./prompt")
     import_modules_from_path("./executors")
 
+
     delay_run(hours=0)
     # 解析命令行参数
     parser = running_paras()
