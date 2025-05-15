d77ed0ef67c796eb073447cd904248d50db14f08
zhengke.gzk <zhengke.gzk@antgroup.com>
2025-05-15 11:50:21 +0800

    1、add outline extractor，and complete test in examples/EastElectric 2、prune duplicate code of markdown_reader

---
+++ kag/examples/EastElectric/builder/ckpt_check.py
@@ -0,0 +1,16 @@
+import json
+import pickle
+from ZODB import DB
+from ZODB.FileStorage import FileStorage
+from kag.common.checkpointer import CheckPointer, CheckpointerManager
+
+checkpointer: CheckPointer = CheckpointerManager.get_checkpointer(
+    {
+        "type": "diskcache",
+        #"ckpt_dir": "ckpt/SchemaFreeExtractor",
+        "ckpt_dir": "ckpt/OutlineExtractor",
+    }
+)
+
+tmp_key = checkpointer.keys()[-1]
+print(checkpointer.read_from_ckpt(tmp_key))
