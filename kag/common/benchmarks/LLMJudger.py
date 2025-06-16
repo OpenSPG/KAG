850cc0fb54daf27846ce516e3b061e8f8069c5f8
wanxingyu.wxy <wanxingyu.wxy@antgroup.com>
2025-06-16 16:24:36 +0800

    bugfix: 取消benchmark的KAG_PROJECT_CONF修改

--- kag/common/benchmarks/LLMJudger.py
+++ kag/common/benchmarks/LLMJudger.py
@@ -1,21 +1,18 @@
 from kag.interface import LLMClient
-from kag.common.conf import KAGConstants, KAGConfigAccessor
+from kag.common.conf import KAGConstants, KAGConfigAccessor, KAG_PROJECT_CONF
 from kag.common.registry.registrable import Registrable
 from kag.common.benchmarks.prompt.JudgerPrompt import JudgerPrompt
 
 
 class LLMJudger(Registrable):
-    def __int__(self, llm: LLMClient = None, **kwargs):
+    def __int__(self, llm: LLMClient = None):
         self.llm = llm
-        task_id = kwargs.get(KAGConstants.KAG_QA_TASK_CONFIG_KEY, None)
-        kag_config = KAGConfigAccessor.get_config(task_id)
-        self.kag_project_config = kag_config.global_config
     """
         judge whether prediction is consistent with gold
     """
 
     def judge_by_llm(self, question: str, prediction: str, gold: str):
-        language = self.kag_project_config.language
+        language = KAG_PROJECT_CONF.language
         prompt = JudgerPrompt(language)
         response = self.llm.invoke(
             {"question": question, "prediction": prediction, "gold": gold}, prompt
