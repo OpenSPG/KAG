f34ef7b3850d4b6e7021324e34d389720d45c4fa
royzhao <peilong.zpl@antgroup.com>
2025-06-26 14:41:01 +0800

    fix(builder): use index manager to commit index schema (#608)
    * fix writer bug
    
    * add index type from indexmanager
    
    * rmove unused index.schema
    
    * add index type from indexmanager
    
    * remove llmrun

--- knext/command/sub_command/schema.py
+++ knext/command/sub_command/schema.py
@@ -12,9 +12,9 @@
 
 import os
 from pathlib import Path
-import yaml
 import click
 import knext.project
+from kag.indexer import KAGIndexManager
 
 from knext.schema.marklang.concept_rule_ml import SPGConceptRuleMarkLang
 from knext.schema.marklang.schema_ml import SPGSchemaMarkLang
@@ -30,21 +30,33 @@ def commit_schema():
         knext.project.DEFAULT_SCHEMA_DIR,
         knext.project.DEFAULT_SCHEMA_FILE.replace("$namespace", env.namespace),
     )
-    index_file = os.path.join(
-        env.project_path,
-        knext.project.DEFAULT_SCHEMA_DIR,
-        knext.project.DEFAULT_INDEX_FILE,
-    )
+
     if Path(schema_file).exists():
         ml = SPGSchemaMarkLang(schema_file, host_addr=env.host_addr, project_id=env.project_id)
     else:
         ml = None
-    if Path(index_file).exists():
-        index_ml = SPGSchemaMarkLang(index_file, host_addr=env.host_addr, project_id=env.project_id)
-    else:
-        index_ml = None
+    index_managers = KAGIndexManager.list_available()
+    index_ml = None
+    for index_manager_name in index_managers:
+        config = {
+            "type": index_manager_name,
+            "llm_config": None,
+            "vectorize_model_config": None,
+        }
+
+        index_mgr = KAGIndexManager.from_config(config)
+        schema_str = index_mgr.schema
+        if not schema_str:
+            continue
+        schema_str = f"namespace {env.namespace}\n" + schema_str
+        cur_index_ml = SPGSchemaMarkLang(filename="", script_data_str=schema_str, host_addr=env.host_addr, project_id=env.project_id)
+        if index_ml is None:
+            index_ml = cur_index_ml
+        else:
+            index_ml.types.update(cur_index_ml.types)
+
     if ml is None and index_ml is None:
-        click.secho(f"ERROR: File {schema_file} and {index_file} not exists.", fg="bright_red")
+        click.secho(f"ERROR: File {schema_file} and index not exists.", fg="bright_red")
         return
     if ml is None:
         ml = index_ml
